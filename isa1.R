05dd257bce71ca7d45d87a85181352d3d548a621
Leo <leojequier@bluewin.ch>
2018-03-15 12:41:40 +0100

    isa a compilé!!!

--- isa1.R
+++ isa1.R
@@ -1,10 +1,13 @@
 #library(isa2)
 #exp = read.csv("data/rpkm.csv", sep = ";")
 #exp = as.matrix(exp)
-head(exp[,1:10])
-data3 = isa(exp[1:100])
+
+
+Sys.time()
+modules2 = isa(exp, thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
 Sys.time()
 
-Sys.time() #pour savoir le temps que prend la fonction
-datai2 = isa(exp, thr.row = c(4, 5, 6), thr.col = c(4,5,6), no.seeds = 200)
+Sys.time() #permet de savoir le temps que prend la fonction pour compiler
+modules = isa(exp)
 Sys.time()
+
