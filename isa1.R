4b2793f45912d422560bd5c7af2c5de31d532f08
Leo <leojequier@bluewin.ch>
2018-03-16 12:37:19 +0100

    save local change

--- isa1.R
+++ isa1.R
@@ -1,15 +1,14 @@
 library(isa2)
 library(lattice)
-
+library(topGO)
 #exp = read.csv("data/rpkm.csv", sep = ";")
 #exp = as.matrix(exp)
-gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
+#gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
+#gene_id = as.array(gene_id[,1]) #n'est plus utile car gene_id est dans rdata
 
 Sys.time()
 #modules2 = isa(exp, thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
 Sys.time()
-
-modules2
 
 Sys.time() #permet de savoir le temps que prend la fonction pour compiler
 #modules = isa(exp)
@@ -17,16 +16,19 @@ Sys.time()
 #modules
 
 #levelplot(as.matrix(modules2))
-images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
+#images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
 
+#rmod_i contiennent les lignes des modules en binaire
 rmod1_i = modules$rows[,1]
 rmod2_i = modules$rows[,2]
 rmod3_i = modules$rows[,3]
 
+#cmod_i contiennent les colonnes des modules en binaire
 cmod1_i = modules$rows[,1]
 cmod2_i = modules$rows[,2]
 cmod3_i = modules$rows[,3]
 
+#rmod et cmod contiennent les indices des gènes de chaque modules 
 rmod1 = which(rmod1_i != 0)
 cmod1 = which(cmod1_i != 0)
 length(rmod1)
@@ -42,11 +44,19 @@ cmod3 = which(cmod3_i != 0)
 length(rmod3)
 length(cmod3)
 
-gene_id = as.array(gene_id[,1])
-gene_id[1:5]
-#le gènes dans les modules 1, 2 et 3
+#les nom des gènes dans les modules 1, 2 et 3
 g_mod1 = gene_id[rmod1]
-g_mod1
 g_mod2 = gene_id[rmod2]
-
 g_mod3 = gene_id[rmod3]
+
+#droplevels
+g_mod1 = droplevels(g_mod1)
+g_mod1 = as.array(g_mod1)
+typeof(c(1:10))
+
+g_mod1[1]
+typeof(g_mod1[1])
+g_mod2 = droplevels(g_mod2)
+g_mod3 = droplevels(g_mod3)
+
+
