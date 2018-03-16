35b88b415309c20dd67d4b0de7cb61c6065c45e6
Afanc <dariush.mollet@gmail.com>
2018-03-16 09:46:57 +0100

    meh

--- isa1.R
+++ isa1.R
@@ -1,13 +1,20 @@
-#library(isa2)
+library(isa2)
+library(lattice)
 #exp = read.csv("data/rpkm.csv", sep = ";")
 #exp = as.matrix(exp)
 
 
 Sys.time()
-modules2 = isa(exp, thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
+#modules2 = isa(exp, thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
 Sys.time()
 
+modules2
+
 Sys.time() #permet de savoir le temps que prend la fonction pour compiler
-modules = isa(exp)
+#modules = isa(exp)
 Sys.time()
+#modules
+
+#levelplot(as.matrix(modules2))
+images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
 
