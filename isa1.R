017bc0518d3246a20ff83d27a3b737b6cf943e2f
Leo <leojequier@bluewin.ch>
2018-03-16 11:32:13 +0100

    gene id complete

--- isa1.R
+++ isa1.R
@@ -18,26 +18,34 @@ Sys.time()
 #levelplot(as.matrix(modules2))
 images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
 
-rmod1 = which(rmod1 != 0)
-cmod1 = which(cmod1 != 0)
-length(which(rmod1 != 0))
-length(which(cmod1 != 0))
+rmod1_i = modules$rows[,1]
+rmod2_i = modules$rows[,2]
+rmod3_i = modules$rows[,3]
 
-rmod2 = which(rmod2 != 0)
-cmod2 = which(cmod2 != 0)
-length(which(rmod2 != 0))
-length(which(cmod2 != 0))
+cmod1_i = modules$rows[,1]
+cmod2_i = modules$rows[,2]
+cmod3_i = modules$rows[,3]
 
-rmod3 = which(rmod3 != 0)
-cmod3 = which(cmod3 != 0)
-length(which(rmod3 != 0))
-length(which(cmod3 != 0))
+rmod1 = which(rmod1_i != 0)
+cmod1 = which(cmod1_i != 0)
+length(rmod1)
+length(rmod1)
+
+rmod2 = which(rmod2_i != 0)
+cmod2 = which(cmod2_i != 0)
+length(rmod2)
+length(cmod2)
+
+rmod3 = which(rmod3_i != 0)
+cmod3 = which(cmod3_i != 0)
+length(rmod3)
+length(cmod3)
 
 gene_id = as.array(gene_id[,1])
 gene_id[1:5]
 #le gènes dans les modules 1, 2 et 3
 g_mod1 = gene_id[rmod1]
-
+g_mod1
 g_mod2 = gene_id[rmod2]
 
 g_mod3 = gene_id[rmod3]
