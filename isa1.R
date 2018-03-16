d20dd8815e00c2172cf41f98c8e3ba404ed99e7a
Afanc <dariush.mollet@gmail.com>
2018-03-16 12:19:06 +0100

    intéressant

--- isa1.R
+++ isa1.R
@@ -1,9 +1,10 @@
 library(isa2)
 library(lattice)
+library(biomaRt)
 
 #exp = read.csv("data/rpkm.csv", sep = ";")
 #exp = as.matrix(exp)
-gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
+#gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
 
 Sys.time()
 #modules2 = isa(exp, thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
@@ -17,7 +18,7 @@ Sys.time()
 #modules
 
 #levelplot(as.matrix(modules2))
-images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
+#images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
 
 rmod1_i = modules$rows[,1]
 rmod2_i = modules$rows[,2]
@@ -42,11 +43,20 @@ cmod3 = which(cmod3_i != 0)
 length(rmod3)
 length(cmod3)
 
-gene_id = as.array(gene_id[,1])
+#gene_id = as.array(gene_id[,1])
 gene_id[1:5]
 #le gènes dans les modules 1, 2 et 3
 g_mod1 = gene_id[rmod1]
 g_mod1
 g_mod2 = gene_id[rmod2]
-
 g_mod3 = gene_id[rmod3]
+
+ensembl=useMart("ensembl")
+ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
+
+infos = getBM(attributes= c("ensembl_gene_id", "external_gene_name","description","interpro_description"), filters="ensembl_gene_id", values=g_mod1, mart=ensembl)
+
+infos
+
+
+
