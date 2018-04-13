1a79bce10156e7d03f9acf88e277ba8f57aabbc4
Leo <leojequier@bluewin.ch>
2018-04-13 16:30:26 +0200

    new robust

--- robustness.R
+++ robustness.R
@@ -1,15 +1,18 @@
-#load("C:/Users/leoje/isa1-2-1")
+load("C:/Users/leoje/isa1-2-1")
 
 robust_out = list()
 
-isa_out[[1]]$seeddata$rob
-
 i = 1
-for(it in isa_out){
-  robu = it$seeddata$rob
+for(it in 1:length(isa_out)){
+  robu = isa_out[[it]]$seeddata$rob
   robust_out[[i]] = robu
   i = i + 1
 }
 
-robust_out[1]
-hist(as.numeric(robust_out[[4]]))
+
+length(isa_out)
+isa_out[[2]]$seeddata$rob
+
+length(robust_out)
+hist(as.numeric(robust_out[[1]]))
+hist(as.numeric(robust_out[[2]]))
