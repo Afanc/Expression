37908a06bb0fa7275efc9455a2bc0642d06b8004
Student 18b [SB] <stu18b@neve.unil.ch>
2018-04-13 13:35:04 +0200

    là ?

--- isaa_0.R
+++ isaa_0.R
@@ -9,36 +9,42 @@ library(biomaRt)
 exp = read.csv("data/rpkm.txt", sep = ",")
 exp = as.matrix(exp)
 
-#normalisation log des données
 epsilon = 0.04
 
-pdf("log_expression.pdf")
-hist(log10(as.vector(as.matrix(exp+epsilon))),100)
-
 log_exp = log10(exp+epsilon)
-zscored_exp = scale(log_exp)
+norm_expr = scale(log_exp)
 
-#Sys.time()
-#modules2 = isa(log10(exp+epsilon), thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
-
-#Sys.time() #permet de savoir le temps que prend la fonction pour compiler
-#modules = isa(exp)
-#Sys.time()
+#pdf("log_expression.pdf")
+#hist(log10(as.vector(as.matrix(exp+epsilon))),100)
 
 
-
-droplevelsthatworks = function(x){
-    return(droplevels(x))
+print("let's goooo\n")
+Sys.time()
+isa_out = list()
+i = 1
+for(threshold in seq(1,2)){
+    m = isa(norm_expr, threshold)
+    isa_out[[i]] = m
+    i = i + 1
+    print(paste("itération ",i, " terminée\n", sep=""))
+    print(Sys.time())
 }
 
-rmod = vector(,length(modules$rows[1,]))
-for(i in length(modules$rows[1,])){
-    rmod[i] = modules$rows[,i]
-    #très moche, pas faire ça
-    assign(paste("rmod", i, sep = ""), which(rmod1_i != 0))
-    assign(paste("g_mod",i, sep = ""), gene_id[i])
-    assign(paste("g_mod",i, sep = ""),  droplevelsthatworks(eval(parse(text = paste("g_mod",i, sep = "")))))
-}
+save.image("images/first_test")
 
-g_mod1
 
+#droplevelsthatworks = function(x){
+    #return(droplevels(x))
+#}
+
+#rmod = vector(,length(modules$rows[1,]))
+#for(i in length(modules$rows[1,])){
+#    rmod[i] = modules$rows[,i]
+#    #très moche, pas faire ça
+#    assign(paste("rmod", i, sep = ""), which(rmod1_i != 0))
+#    assign(paste("g_mod",i, sep = ""), gene_id[i])
+#    assign(paste("g_mod",i, sep = ""),  droplevelsthatworks(eval(parse(text = paste("g_mod",i, sep = "")))))
+#}
+#
+#g_mod1
+#
