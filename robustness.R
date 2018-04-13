514d5eda2818ac8fcdf1e313c69c802b5468bb61
Leo <leojequier@bluewin.ch>
2018-04-13 14:59:28 +0200

    robustness

--- robustness.R
+++ robustness.R
@@ -1,18 +1,6 @@
-load("images/hello.Rdata")
-
-isa_out = vector(mode = "list")
-i = 1
-for(threshold in seq(1,2)){
-  isa_out[[i]] = paste("t_", threshold, sep = "")
-  assign(paste("t_", threshold, sep = ""),seq(1:10))
-  i = i + 1
-}
-
-isa_out[[1]]
+load("images/isa_out.Rdata")
 
 robust_out = list()
-
-i = 1
 
 for(it in isa_out){
   rob = it$seeddata[6]
@@ -21,13 +9,3 @@ for(it in isa_out){
 }
 
 
-test = vector(mode = "list", length = 3)
-
-for( i in 1:3){ 
-  test[[i]] = paste("l", i, sep = "_")
-  assign(paste("l", i, sep = "_"), 1:10)
-  }
-
-for(i in test){
-  print(i)
-}
