51115f9133d66123bcd76379f9e172f471c64f48
Student 18b [SB] <stu18b@neve.unil.ch>
2018-04-13 11:05:43 +0200

    aeusnth

--- isaa_0.R
+++ isaa_0.R
@@ -6,33 +6,27 @@ source("https://bioconductor.org/biocLite.R")
 library(biomaRt)
 
 
-#exp = read.csv("data/rpkm.csv", sep = ";")
-#exp = as.matrix(exp)
-#gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
-#gene_id = as.array(gene_id[,1]) #n'est plus utile car gene_id est dans rdata
-
-#exp = read.csv("data/rpkm.csv", sep = ";",header=False)
-#exp = as.matrix(exp)
-#gene_id = read.csv("C:/Users/leoje/Dropbox/uni/semestre 6/math/data/geneID.txt", sep = " ")
+exp = read.csv("data/rpkm.csv", sep = ",")
+exp = as.matrix(exp)
 
 #normalisation log des données
 epsilon = 0.04
+
+pdf("log_expression.pdf")
+head(exp)
+exp+epsilon
 hist(log10(as.vector(as.matrix(exp+epsilon))),100)
 
 log_exp = log10(exp+epsilon)
 zscored_exp = scale(log_exp)
 
-#isa
-Sys.time()
+#Sys.time()
 #modules2 = isa(log10(exp+epsilon), thr.row = c(0.5, 1, 1.5, 2), thr.col = c(0.5, 1, 1.5, 2))
-Sys.time()
 
 #Sys.time() #permet de savoir le temps que prend la fonction pour compiler
 #modules = isa(exp)
 #Sys.time()
 
-#levelplot(as.matrix(modules2))
-#images(as.matrix(modules2$rows), strip = FALSE, xlab = "", ylab = "")
 
 
 droplevelsthatworks = function(x){
@@ -50,11 +44,3 @@ for(i in length(modules$rows[1,])){
 
 g_mod1
 
-#cmod_i contiennent les colonnes des modules en binaire
-#PAS ENCORE
-#cmod1_i = modules$rows[,1]
-#cmod2_i = modules$rows[,2]
-#cmod3_i = modules$rows[,3]
-
-#les nom des gènes dans les modules 1, 2 et 3
-#le gènes dans les modules 1, 2 et 3
