0fd1e7b52b1774f37a9e1248301d77b76703b785
Leo <leojequier@bluewin.ch>
2018-04-13 16:26:38 +0200

    robustesse avec hist

--- robustness.R
+++ robustness.R
@@ -1,11 +1,15 @@
-load("images/isa_out.Rdata")
+#load("C:/Users/leoje/isa1-2-1")
 
 robust_out = list()
 
+isa_out[[1]]$seeddata$rob
+
+i = 1
 for(it in isa_out){
-  rob = it$seeddata[6]
-  robust_out[[i]] = rob
+  robu = it$seeddata$rob
+  robust_out[[i]] = robu
   i = i + 1
 }
 
-
+robust_out[1]
+hist(as.numeric(robust_out[[4]]))
