1eb8c2e673a951199fb5b2e3fd82e143ed2cfb7c
Afanc <dariush.mollet@gmail.com>
2018-04-13 17:28:02 +0200

    rob. good !

--- robustness.R
+++ robustness.R
@@ -15,24 +15,16 @@ for(it in 1:length(isa_out)){
 #length(isa_out)
 #isa_out[[2]]$seeddata$rob
 
-<<<<<<< HEAD
-summary(robust_out[[1]])
-summary(robust_out[[2]])
-boxplot(robust_out)
-
-save.image("images/rob-1-2-1")
-=======
 for(i in 1:length(isa_out)){
     summary(robust_out[[i]])
 }
->>>>>>> cfd6f98b26d5ec77834c54f171f9e35841be6cde
 
 pdf("results/rob1-2-1.pdf")
 
-boxplot(robust_out, xlab=thresholdzz)
+boxplot(robust_out, names=thresholdzz, main="robust vs thresholds")
 
 for(i in 1:length(isa_out)){
-    hist(as.numeric(robust_out[[i]]))
+    hist(as.numeric(robust_out[[i]]), main=paste("robustness = ", i, sep=""))
 }
 
 dev.off()
