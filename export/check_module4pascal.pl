c1575eaefb68140bd39d70d817ba2b536c7a4cb9
Afanc <dariush.mollet@gmail.com>
2018-05-12 12:09:45 +0200

    shit

--- export/check_module4pascal.pl
+++ export/check_module4pascal.pl
@@ -10,8 +10,21 @@ open(my $modules1, '<', './module4pascal_d.txt') or die "could not open";
 
 while(my $line = <$modules1>) {
     my @mod1 = split /\t/, $line;
-    print $mod1[0].", size : ".scalar @mod1."\n";
-    #print $line;   
+    my $num_modules = scalar @mod1 - 2;
+    print $mod1[0].", size : ".$num_modules."\n";
+
+
+    my @filtered = uniq(@mod1);
+    if (@filtered != @mod1) {
+        print '\n ALERT !! \n';
+    }
 }
 
 close $modules1;
+
+#array with uniques only
+sub uniq {
+    my %seen;
+    grep !$seen{$_}++, @_;
+}
+
