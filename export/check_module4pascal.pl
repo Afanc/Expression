f77eb81f265250922f19e43c21281dc7ee12e7c8
Afanc <dariush.mollet@gmail.com>
2018-05-12 09:56:36 +0200

    perl scripts

---
+++ export/check_module4pascal.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open(my $modules1, '<', './module4pascal_d.txt') or die "could not open";
+
+my $row = <$modules1>;
+my @mod1 = split /\t/, $row;
+#print scalar @mod1;
+
+while(my $line = <$modules1>) {
+    my @mod1 = split /\t/, $line;
+    print $mod1[0].", size : ".scalar @mod1."\n";
+    #print $line;   
+}
+
+close $modules1;
