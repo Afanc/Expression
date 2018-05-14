5b58cb6c7cddad57cbfd185b938d08ef0c87f1cb
Leo <leojequier@bluewin.ch>
2018-05-14 16:55:01 +0200

    pascal analysis

--- pascal result/pascal analysis.R
+++ pascal result/pascal analysis.R
@@ -1,9 +1,40 @@
-read.table("EUR.ASN.C4D_CAD_discovery_metaanalysis_update.CAD.PathwaySet--module4pascal--sum.txt",
-           header = T, 
-           sep = "\t")
-data = read.table("EUR.ASN.C4D_CAD_discovery_metaanalysis_update.CAD.sum.genescores.txt", header = T)
+filelist = list.files(pattern = ".*.txt")
 
-head(data)
-which(data$pvalue < 0.05/length(data$pvalue))
+#assuming tab separated values with a header    
+datalist = lapply(filelist, function(x)read.table(x, header=T)) 
 
-data$gene_id[data$pvalue < 0.05/length(data$pvalue)]
+arglist = list()
+arglist[[1]] = "Name"
+names(arglist) = c("by")
+
+length(datalist)
+merged = datalist[[1]]
+i = 2
+for(i in 2:10){
+  merged = merge(merged, datalist[i], by = "Name")
+
+}
+mergenames = c("Names", paste(rep(substr(filelist[1:10],start = 5, stop = 25),each = 2), rep(c("chisq", "pval"), times = 10)))
+names(merged) = mergenames
+head(merged)
+
+
+pval = as.matrix(merged[,seq(1, 21, by = 2)])
+
+alpha = 0.05/(61)
+
+which(as.vector(pval[,2:11]) < alpha)
+
+#tentative de FDR
+
+sorted = sort(as.vector(pval[,2:11]), decreasing = F)
+
+head(sorted)
+
+for(i in 1:length(sorted)){
+  if(sorted[i] > (i/length(sorted))* 0.05){
+    return(paste("result", i-1))
+    break()
+  }
+}
+
