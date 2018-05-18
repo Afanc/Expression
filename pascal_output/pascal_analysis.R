1590ab36d4542d3668690d227af14c008177fb45
Leo <leojequier@bluewin.ch>
2018-05-18 11:33:53 +0200

    pasca analysis ok

--- pascal_output/pascal_analysis.R
+++ pascal_output/pascal_analysis.R
@@ -1,31 +1,27 @@
 #!/usr/bin/R
 
-filelist = list.files(pattern = ".*.txt")
+filelist = list.files(pattern = ".module4pascal_d--sum.txt")
 
 #assuming tab separated values with a header    
 datalist = lapply(filelist, function(x)read.table(x, header=T)) 
 
-arglist = list()
-arglist[[1]] = "Name"
-names(arglist) = c("by")
 
 length(datalist)
 merged = datalist[[1]]
-i = 2
-for(i in 2:10){
+for(i in 2:length(datalist)){
   merged = merge(merged, datalist[i], by = "Name")
-
 }
-mergenames = c("Names", paste(rep(substr(filelist[1:10],start = 5, stop = 25),each = 2), rep(c("chisq", "pval"), times = 10)))
+
+mergenames = c("Names", paste(rep(substr(filelist[1:length(filelist)],start = 5, stop = 25),each = 2), rep(c("chisq", "pval"), times = 10)))
 names(merged) = mergenames
 head(merged)
 
 
-pval = as.matrix(merged[,seq(1, 21, by = 2)])
+pval = as.matrix(merged[,seq(1, 35, by = 2)])
 
 alpha = 0.05/(61)
 
-which(as.vector(pval[,2:11]) < alpha)
+which(as.vector(pval[,2:18]) < alpha)
 
 #tentative de FDR
 
