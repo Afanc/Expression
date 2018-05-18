5e56f084dca03bac37bbad60ea5faaad65daba55
Afanc <dariush.mollet@gmail.com>
2018-05-18 12:23:16 +0200

    YES correct entrez shit

--- treating_isa_output/convert.pl
+++ treating_isa_output/convert.pl
@@ -5,19 +5,28 @@ use warnings;
 
 open(my $old_file, 'geneID.txt') or die "could not open";
 
+
 while (my $line = <$old_file>) {
     print $line;
     chomp($line);
 
     open(my $new_file, '>>', 'entrez_ids_2.txt') or die 'could not write';
     open(my $cipher, 'geneid2.txt') or die 'could not openn';
+
+    my $found_cipher = 0;
+
     while (my $cipher_line = <$cipher>) {
         if ($cipher_line =~ m/$line/){
             my @part = split /\t/, $cipher_line;
             print $part[1]."\n";
             print $new_file $part[1]."\n";
+            $found_cipher = 1;
         }
     }
+    if ($found_cipher == 0){
+        print $new_file 'NA'."\n";
+    }
+
     close $new_file;
     close $cipher;
 }
