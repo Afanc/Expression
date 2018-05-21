fb8071860d49e717a1697c99f2a2494e3bbf89e2
Afanc <dariush.mollet@gmail.com>
2018-05-21 17:43:12 +0200

    that is so wrong

--- corrected_pascal_output/pascal_analysis_old.R
+++ corrected_pascal_output/pascal_analysis_old.R
@@ -5,45 +5,40 @@ filelist = list.files(pattern = ".module4pascal--sum.txt")
 #assuming tab separated values with a header    
 datalist = lapply(filelist, function(x)read.table(x, header=T)) 
 
-length(datalist)
 merged = datalist[[1]]
 for(i in 2:length(datalist)){
   merged = merge(merged, datalist[i], by = "Name")
 }
 
 mergenames = c("Names", paste(rep(substr(filelist[1:length(filelist)],start = 5, stop = 25),each = 2), rep(c("chisq", "pval"), times = 10)))
-names(merged) = mergenames
-
 merged
+names(merged) = mergenames
 
 pval = as.matrix(merged[,seq(1, 35, by = 2)])
 chisq = as.matrix(merged[,seq(2, 34, by = 2)])
 
 sorted = sort(as.vector(pval[,2:11]), decreasing = F)
-sorted_chisq = sort(as.vector(chisq[,2:11]), decreasing = F)
+sorted_chisq = sort(as.vector(chisq[,1:11]), decreasing = F)
 
 sorted = as.numeric(sorted)
 sorted_chisq = as.numeric(sorted_chisq)
 
 correction_factor = 61
 
-head(sorted*correction_factor)
+head(sorted_chisq)
 head(sorted_chisq*correction_factor)
 
-pval[,2:ncol(pval)]
-chisq[,2:ncol(chisq)]
-
+head(merged)
+merged[[1]]
 for(i in 1:10){
     a = which(as.vector(chisq[,2:ncol(chisq)]) == sorted_chisq[i])
     col = floor(a / correction_factor) + 2 #r magic <3
     mod = a - ((col-2) * correction_factor)
-    print(chisq[mod,1])
+    print("Module : ")
+    print(chisq[mod,col])
+    print(droplevels(merged[[1]][col]))
 }
 
-#le nom des modules
-for(i in 1:10){
-    a = which(as.vector(pval[,2:ncol(pval)]) == sorted[i])
-    col = floor(a / correction_factor) + 2 #r magic <3
-    mod = a - ((col-2) * correction_factor)
-    print(pval[mod,1])
-}
+#pval[,2:ncol(pval)]
+#chisq[,2:ncol(chisq)]
+
